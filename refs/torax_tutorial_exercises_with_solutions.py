8f745f539c23c9788bc5a53ee358224523d30575
Sergey Litvinov <slitvinov@gmail.com>
2025-09-25 19:35:34 -0400

    up

--- refs/torax_tutorial_exercises_with_solutions.py
+++ refs/torax_tutorial_exercises_with_solutions.py
@@ -1,37 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # TORAX Tutorial with Solutions
-# <a href="https://colab.research.google.com/github/google-deepmind/torax/blob/main/torax/tutorials/torax_tutorial_exercises_with_solutions.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
-#
-
-# In[ ]:
-
-#@title LICENSE
-# Copyright 2024 DeepMind Technologies Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# In[ ]:
-
-#@title Install TORAX
-# This installs the latest TORAX version from https://pypi.org/project/torax.
-# To install the current version on GitHub, use `%pip install git+https://github.com/google-deepmind/torax.git`.
-get_ipython().run_line_magic('pip', 'install torax')
-
-# In[ ]:
-
-#@title Imports
 import copy
 import jax
 import torax
