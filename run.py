4da281c41cc5d84b77afc98535ed91f786adaea3
Sergey Litvinov <slitvinov@gmail.com>
2025-09-25 20:18:29 -0400

    run.py

---
+++ run.py
@@ -0,0 +1,140 @@
+import torax
+
+CONFIG = {
+    'plasma_composition': {
+        'main_ion': {
+            'D': 0.5,
+            'T': 0.5
+        },
+        'impurity': 'Ne',
+        'Z_eff': 1.6,
+    },
+    'profile_conditions': {
+        'Ip': 10.5e6,
+        'T_i': {
+            0.0: {
+                0.0: 15.0,
+                1.0: 0.2
+            }
+        },
+        'T_i_right_bc': 0.2,
+        'T_e': {
+            0.0: {
+                0.0: 15.0,
+                1.0: 0.2
+            }
+        },
+        'T_e_right_bc': 0.2,
+        'n_e_right_bc': 0.25e20,
+        'n_e_nbar_is_fGW': True,
+        'normalize_n_e_to_nbar': True,
+        'nbar': 0.8,
+        'n_e': {
+            0: {
+                0.0: 1.5,
+                1.0: 1.0
+            }
+        },
+    },
+    'numerics': {
+        't_final': 5,
+        'resistivity_multiplier': 200,
+        'evolve_ion_heat': True,
+        'evolve_electron_heat': True,
+        'evolve_current': True,
+        'evolve_density': True,
+        'max_dt': 0.5,
+        'chi_timestep_prefactor': 50,
+        'dt_reduction_factor': 3,
+    },
+    'geometry': {
+        'geometry_type': 'chease',
+        'geometry_file': 'ITER_hybrid_citrin_equil_cheasedata.mat2cols',
+        'Ip_from_parameters': True,
+        'R_major': 6.2,
+        'a_minor': 2.0,
+        'B_0': 5.3,
+    },
+    'neoclassical': {
+        'bootstrap_current': {
+            'bootstrap_multiplier': 1.0,
+        },
+    },
+    'sources': {
+        'generic_current': {
+            'fraction_of_total_current': 0.46,
+            'gaussian_width': 0.075,
+            'gaussian_location': 0.36,
+        },
+        'generic_particle': {
+            'S_total': 2.05e20,
+            'deposition_location': 0.3,
+            'particle_width': 0.25,
+        },
+        'gas_puff': {
+            'puff_decay_length': 0.3,
+            'S_total': 6.0e21,
+        },
+        'pellet': {
+            'S_total': 0.0e22,
+            'pellet_width': 0.1,
+            'pellet_deposition_location': 0.85,
+        },
+        'generic_heat': {
+            'gaussian_location': 0.12741589640723575,
+            'gaussian_width': 0.07280908366127758,
+            'P_total': 51.0e6,
+            'electron_heat_fraction': 0.68,
+        },
+        'fusion': {},
+        'ei_exchange': {
+            'Qei_multiplier': 1.0,
+        },
+    },
+    'pedestal': {
+        'model_name': 'set_T_ped_n_ped',
+        'set_pedestal': True,
+        'T_i_ped': 4.5,
+        'T_e_ped': 4.5,
+        'n_e_ped': 0.62e20,
+        'rho_norm_ped_top': 0.9,
+    },
+    'transport': {
+        'model_name': 'qlknn',
+        'apply_inner_patch': True,
+        'D_e_inner': 0.25,
+        'V_e_inner': 0.0,
+        'chi_i_inner': 1.0,
+        'chi_e_inner': 1.0,
+        'rho_inner': 0.2,
+        'apply_outer_patch': True,
+        'D_e_outer': 0.1,
+        'V_e_outer': 0.0,
+        'chi_i_outer': 2.0,
+        'chi_e_outer': 2.0,
+        'rho_outer': 0.9,
+        'chi_min': 0.05,
+        'chi_max': 100,
+        'D_e_min': 0.05,
+        'DV_effective': True,
+        'include_ITG': True,
+        'include_TEM': True,
+        'include_ETG': True,
+        'avoid_big_negative_s': True,
+        'An_min': 0.05,
+        'ITG_flux_ratio_correction': 1,
+    },
+    'solver': {
+        'solver_type': 'linear',
+        'use_predictor_corrector': True,
+        'n_corrector_steps': 1,
+        'chi_pereverzev': 30,
+        'D_pereverzev': 15,
+        'use_pereverzev': True,
+    },
+    'time_step_calculator': {
+        'calculator_type': 'chi',
+    },
+}
+torax_config = torax.ToraxConfig.from_dict(CONFIG)
+data_tree, _ = torax.run_simulation(torax_config, log_timestep_info=False)
