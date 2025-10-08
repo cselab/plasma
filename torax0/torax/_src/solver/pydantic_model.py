23607794c392d4d247727d7673b7f69f11739ce8
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-07 21:26:23 -0400

    rm nonlinear_theta_method

--- torax0/torax/_src/solver/pydantic_model.py
+++ torax0/torax/_src/solver/pydantic_model.py
@@ -21,7 +21,6 @@ import pydantic
 from torax._src import physics_models as physics_models_lib
 from torax._src.fvm import enums
 from torax._src.solver import linear_theta_method
-from torax._src.solver import nonlinear_theta_method
 from torax._src.solver import runtime_params
 from torax._src.solver import solver as solver_lib
 from torax._src.torax_pydantic import torax_pydantic
@@ -30,25 +29,6 @@ from torax._src.torax_pydantic import torax_pydantic
 
 
 class BaseSolver(torax_pydantic.BaseModelFrozen, abc.ABC):
-  """Base class for solver configs.
-
-  Attributes:
-    theta_implicit: The theta value in the theta method 0 = explicit, 1 = fully
-      implicit, 0.5 = Crank-Nicolson.
-    use_predictor_corrector: Enables use_predictor_corrector iterations with the
-      linear solver. If False, compilation is faster.
-    n_corrector_steps: The number of corrector steps for the predictor-corrector
-      linear solver. 0 means a pure linear solve with no corrector steps.
-    convection_dirichlet_mode: See `fvm.convection_terms` docstring,
-      `dirichlet_mode` argument.
-    convection_neumann_mode: See `fvm.convection_terms` docstring,
-      `neumann_mode` argument.
-    use_pereverzev: Use pereverzev terms for linear solver. Is only applied in
-      the nonlinear solver for the optional initial guess from the linear solver
-    chi_pereverzev: (deliberately) large heat conductivity for Pereverzev rule.
-    D_pereverzev: (deliberately) large particle diffusion for Pereverzev rule.
-  """
-
   theta_implicit: Annotated[
       torax_pydantic.UnitInterval, torax_pydantic.JAX_STATIC
   ] = 1.0
@@ -150,83 +130,6 @@ class NewtonRaphsonThetaMethod(BaseSolver):
   delta_reduction_factor: float = 0.5
   tau_min: float = 0.01
 
-  @functools.cached_property
-  def build_runtime_params(
-      self,
-  ) -> nonlinear_theta_method.NewtonRaphsonRuntimeParams:
-    return nonlinear_theta_method.NewtonRaphsonRuntimeParams(
-        theta_implicit=self.theta_implicit,
-        convection_dirichlet_mode=self.convection_dirichlet_mode,
-        convection_neumann_mode=self.convection_neumann_mode,
-        use_pereverzev=self.use_pereverzev,
-        use_predictor_corrector=self.use_predictor_corrector,
-        chi_pereverzev=self.chi_pereverzev,
-        D_pereverzev=self.D_pereverzev,
-        maxiter=self.n_max_iterations,
-        residual_tol=self.residual_tol,
-        residual_coarse_tol=self.residual_coarse_tol,
-        n_corrector_steps=self.n_corrector_steps,
-        delta_reduction_factor=self.delta_reduction_factor,
-        tau_min=self.tau_min,
-        initial_guess_mode=self.initial_guess_mode.value,
-        log_iterations=self.log_iterations,
-    )
-
-  def build_solver(
-      self,
-      physics_models: physics_models_lib.PhysicsModels,
-  ) -> nonlinear_theta_method.NewtonRaphsonThetaMethod:
-    return nonlinear_theta_method.NewtonRaphsonThetaMethod(
-        physics_models=physics_models,
-    )
-
-
-class OptimizerThetaMethod(BaseSolver):
-  """Model for nonlinear OptimizerThetaMethod solver.
-
-  Attributes:
-    solver_type: The type of solver to use, hardcoded to 'optimizer'.
-    initial_guess_mode: The initial guess mode for the optimizer.
-    n_max_iterations: The maximum number of iterations for the optimizer.
-    residual_tol: The tolerance for the optimizer.
-  """
-
-  solver_type: Annotated[Literal['optimizer'], torax_pydantic.JAX_STATIC] = (
-      'optimizer'
-  )
-  initial_guess_mode: Annotated[
-      enums.InitialGuessMode, torax_pydantic.JAX_STATIC
-  ] = enums.InitialGuessMode.LINEAR
-  n_max_iterations: pydantic.NonNegativeInt = 100
-  loss_tol: float = 1e-10
-
-  @functools.cached_property
-  def build_runtime_params(
-      self,
-  ) -> nonlinear_theta_method.OptimizerRuntimeParams:
-    return nonlinear_theta_method.OptimizerRuntimeParams(
-        theta_implicit=self.theta_implicit,
-        convection_dirichlet_mode=self.convection_dirichlet_mode,
-        convection_neumann_mode=self.convection_neumann_mode,
-        use_pereverzev=self.use_pereverzev,
-        use_predictor_corrector=self.use_predictor_corrector,
-        chi_pereverzev=self.chi_pereverzev,
-        D_pereverzev=self.D_pereverzev,
-        n_max_iterations=self.n_max_iterations,
-        loss_tol=self.loss_tol,
-        n_corrector_steps=self.n_corrector_steps,
-        initial_guess_mode=self.initial_guess_mode.value,
-    )
-
-  def build_solver(
-      self,
-      physics_models: physics_models_lib.PhysicsModels,
-  ) -> nonlinear_theta_method.OptimizerThetaMethod:
-    return nonlinear_theta_method.OptimizerThetaMethod(
-        physics_models=physics_models,
-    )
-
-
 SolverConfig = (
-    LinearThetaMethod | NewtonRaphsonThetaMethod | OptimizerThetaMethod
+    LinearThetaMethod | NewtonRaphsonThetaMethod
 )
