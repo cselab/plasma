d5fb1a70ef3589fa6137396c99241d15d68fb7d8
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-08 05:51:01 -0400

    p

--- torax0/torax/_src/geometry/geometry_loader.py
+++ torax0/torax/_src/geometry/geometry_loader.py
@@ -57,41 +57,6 @@ def _load_CHEASE_data(  # pylint: disable=invalid-name
       var_label: np.asarray(chease_data[var_label]) for var_label in chease_data
   }
 
-
-def _load_fbt_data(file_path: str | IO[bytes]) -> dict[str, np.ndarray]:
-  """Loads data into a dictionary from an FBT-LY file or file path."""
-
-  meq_data = scipy.io.loadmat(file_path, squeeze_me=True)
-
-  if 'LY' not in meq_data:
-    # L file or LY file not the output of MEQ meqlpack. Return as is.
-    return meq_data
-  else:
-    # LY bundle file. numpy structured array likely resulting from
-    # scipy.io.loadmat returning the output of MEQ meqlpack.m.
-    # Reformat to return a dict. Shapes of 2D arrays are (radius, time)
-    LY_bundle_dict = {}  # pylint: disable=invalid-name
-    LY_bundle_array = meq_data['LY']  # pylint: disable=invalid-name
-    field_names = LY_bundle_array.dtype.names
-    for name in field_names:
-      LY_bundle_dict[name] = LY_bundle_array[name].item()
-
-    return LY_bundle_dict
-
-
-def _load_eqdsk_data(file_path: str) -> dict[str, np.ndarray]:
-  eqdsk_data = eqdsk.EQDSKInterface.from_file(file_path, no_cocos=True)
-  # TODO(b/335204606): handle COCOS shenanigans
-  logging.warning(
-      """
-               WARNING: Using EQDSK geometry.
-               The TORAX EQDSK converter has only been tested against CHEASE-generated EQDSK which is COCOS=2.
-               The converter is not guaranteed to work as expected with arbitrary EQDSK input. Please verify carefully.
-               Future work will be done to correctly handle EQDSK inputs provided with a specific COCOS value."""
-  )
-  return eqdsk_data.__dict__  # dict(eqdsk_data)
-
-
 def get_geometry_dir(geometry_dir: str | None = None) -> str:
   """Gets the default geometry directory if no geometry_dir is provided."""
   if geometry_dir is None:
@@ -112,9 +77,5 @@ def load_geo_data(
   match geometry_source:
     case GeometrySource.CHEASE:
       return _load_CHEASE_data(file_path=filepath)
-    case GeometrySource.FBT:
-      return _load_fbt_data(file_path=filepath)
-    case GeometrySource.EQDSK:
-      return _load_eqdsk_data(file_path=filepath)
     case _:
       raise ValueError(f'Unknown geometry source: {geometry_source}')
