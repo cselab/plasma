7e3b8ab10387138bf790a6f874aafd2151e7f93a
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-08 10:16:02 -0400

    up

--- torax0/torax/_src/jax_utils.py
+++ torax0/torax/_src/jax_utils.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Commonly repeated jax expressions."""
 
 import functools
@@ -30,87 +29,24 @@ _State = ParamSpec('_State')
 
 
 @functools.cache
-def get_dtype() -> type(jnp.float32):
-  # Default TORAX JAX precision is f64
-  precision = os.getenv('JAX_PRECISION', 'f64')
-  assert precision == 'f64' or precision == 'f32', (
-      'Unknown JAX precision environment variable: %s' % precision
-  )
-  return jnp.float64 if precision == 'f64' else jnp.float32
+def get_dtype():
+    precision = os.getenv('JAX_PRECISION', 'f64')
+    return jnp.float64 if precision == 'f64' else jnp.float32
 
 
 @functools.cache
-def get_np_dtype() -> type(np.float32):
-  # Default TORAX JAX precision is f64
-  precision = os.getenv('JAX_PRECISION', 'f64')
-  assert precision == 'f64' or precision == 'f32', (
-      'Unknown JAX precision environment variable: %s' % precision
-  )
-  return np.float64 if precision == 'f64' else np.float32
+def get_np_dtype():
+    precision = os.getenv('JAX_PRECISION', 'f64')
+    return np.float64 if precision == 'f64' else np.float32
 
 
-def env_bool(name: str, default: bool) -> bool:
-  """Get a bool from an environment variable.
-
-  Args:
-    name: The name of the environment variable.
-    default: The default value of the bool.
-
-  Returns:
-    value: The value of the bool.
-  """
-  if name not in os.environ:
-    return default
-  str_value = os.environ[name]
-  if str_value in ['1', 'True', 'true']:
-    return True
-  if str_value in ['0', 'False', 'false']:
-    return False
-  raise ValueError(f'Unrecognized boolean string {str_value}.')
-
-
-# If True, `error_if` functions will raise errors.  Otherwise they are
-# pass throughs.
-# Default to False, because host_callbacks are incompatible with the
-# persistent compilation cache.
-_ERRORS_ENABLED: bool = env_bool('TORAX_ERRORS_ENABLED', False)
-
-
-def error_if(
-    var: jax.Array,
-    cond: jax.Array,
-    msg: str,
-) -> jax.Array:
-  """Raises error if cond is true, and `errors_enabled` is True.
-
-  This is just a wrapper around `equinox.error_if`, gated by `errors_enabled`.
-
-  Args:
-    var: The variable to pass through.
-    cond: Boolean array, error if cond is true.
-    msg: Message to print on error.
-
-  Returns:
-    var: Identity wrapper that must be used for the check to be included.
-  """
-  if not _ERRORS_ENABLED:
+def error_if(var, cond, msg):
     return var
-  return eqx.error_if(var, cond, msg)
 
 
-def assert_rank(
-    inputs: chex.Numeric | jax.stages.ArgInfo,
-    rank: int,
-) -> None:
-  """Wrapper around chex.assert_rank that supports jax.stages.ArgInfo."""
-  if isinstance(inputs, jax.stages.ArgInfo):
-    chex.assert_rank(inputs.shape, rank)
-  else:
+def assert_rank(inputs, rank):
     chex.assert_rank(inputs, rank)
 
 
-def jit(*args, **kwargs) -> Callable[..., Any]:
-  """Calls jax.jit if TORAX_COMPILATION_ENABLED is True, otherwise no-op."""
-  if env_bool('TORAX_COMPILATION_ENABLED', True):
+def jit(*args, **kwargs):
     return jax.jit(*args, **kwargs)
-  return args[0]
