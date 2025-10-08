f92968326ab75a741c5db740a6fcb955b3e75e57
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-08 12:22:53 -0400

    up

--- torax0/torax/_src/torax_pydantic/pydantic_types.py
+++ torax0/torax/_src/torax_pydantic/pydantic_types.py
@@ -15,13 +15,6 @@ def _numpy_array_before_validator(
     x: np.ndarray | NumpySerialized, ) -> np.ndarray:
     if isinstance(x, np.ndarray):
         return x
-    elif isinstance(x, tuple) or isinstance(x, list) and len(x) == 2:
-        dtype, data = x
-        return np.array(data, dtype=np.dtype(dtype))
-    else:
-        raise ValueError(
-            'Expected NumPy or a tuple representing a serialized NumPy array, but'
-            f' got a {type(x)}')
 
 
 def _numpy_array_serializer(x: np.ndarray) -> NumpySerialized:
@@ -29,14 +22,10 @@ def _numpy_array_serializer(x: np.ndarray) -> NumpySerialized:
 
 
 def _numpy_array_is_rank_1(x: np.ndarray) -> np.ndarray:
-    if x.ndim != 1:
-        raise ValueError(f'NumPy array is not 1D, rather of rank {x.ndim}')
     return x
 
 
 def _numpy_array_is_sorted(x: np.ndarray) -> np.ndarray:
-    if not np.all(x[:-1] <= x[1:]):
-        raise ValueError(f'NumPy array is not sorted: {x}')
     return x
 
 
@@ -53,9 +42,6 @@ NumpyArray1DSorted = Annotated[NumpyArray,
 
 
 def _array_is_unit_interval(array: np.ndarray) -> np.ndarray:
-    if not np.all((array >= 0.0) & (array <= 1.0)):
-        raise ValueError(
-            f'Some array elements are not in the unit interval: {array}')
     return array
 
 
