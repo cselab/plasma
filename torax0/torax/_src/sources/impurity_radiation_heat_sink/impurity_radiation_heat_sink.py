129b515f177170f6caf367e4520c63ecb26a0886
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-08 17:20:16 -0400

    up

--- torax0/torax/_src/sources/impurity_radiation_heat_sink/impurity_radiation_heat_sink.py
+++ torax0/torax/_src/sources/impurity_radiation_heat_sink/impurity_radiation_heat_sink.py
@@ -6,12 +6,3 @@ from torax._src.sources import source as source_lib
 class ImpurityRadiationHeatSink(source_lib.Source):
     SOURCE_NAME = "impurity_radiation"
     model_func: source_lib.SourceProfileFunction
-
-    @property
-    def source_name(self) -> str:
-        return self.SOURCE_NAME
-
-    @property
-    def affected_core_profiles(
-        self, ) -> tuple[source_lib.AffectedCoreProfile, ...]:
-        return (source_lib.AffectedCoreProfile.TEMP_EL, )
