c8f221bb4a4dcb72964669f0ac7871f40ec3dd36
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-08 21:10:04 -0400

    rm pydantic_model

--- run.py
+++ run.py
@@ -31,7 +31,6 @@ from torax._src.neoclassical.conductivity import base as conductivity_base
 from torax._src.neoclassical.conductivity import sauter as sauter_conductivity
 from torax._src.neoclassical.transport import zeros as transport_zeros
 from torax._src.pedestal_model import pedestal_model as pedestal_model_lib
-from torax._src.pedestal_model import pydantic_model as pedestal_pydantic_model
 from torax._src.physics import charge_states
 from torax._src.physics import collisions
 from torax._src.physics import formulas
@@ -78,19 +77,58 @@ import numpy as np
 import pydantic
 import typing_extensions
 import xarray as xr
-import dataclasses
+import abc
+from typing import Annotated, Literal
 import chex
-import jax
-from jax import numpy as jnp
-from torax._src import array_typing
-from torax._src import constants
-from torax._src.config import runtime_validation_utils
+from torax._src.pedestal_model import pedestal_model
+from torax._src.pedestal_model import set_tped_nped
 from torax._src.torax_pydantic import torax_pydantic
-from typing_extensions import Final
+
+
+class BasePedestal(torax_pydantic.BaseModelFrozen, abc.ABC):
+    set_pedestal: torax_pydantic.TimeVaryingScalar = (
+        torax_pydantic.ValidatedDefault(False))
+
+    @abc.abstractmethod
+    def build_pedestal_model(self):
+        pass
+
+    @abc.abstractmethod
+    def build_runtime_params(self, t):
+        pass
+
+
+class SetTpedNped(BasePedestal):
+    model_name: Annotated[Literal['set_T_ped_n_ped'],
+                          torax_pydantic.JAX_STATIC] = 'set_T_ped_n_ped'
+    n_e_ped: torax_pydantic.TimeVaryingScalar = torax_pydantic.ValidatedDefault(
+        0.7e20)
+    n_e_ped_is_fGW: bool = False
+    T_i_ped: torax_pydantic.TimeVaryingScalar = torax_pydantic.ValidatedDefault(
+        5.0)
+    T_e_ped: torax_pydantic.TimeVaryingScalar = torax_pydantic.ValidatedDefault(
+        5.0)
+    rho_norm_ped_top: torax_pydantic.TimeVaryingScalar = (
+        torax_pydantic.ValidatedDefault(0.91))
+
+    def build_pedestal_model(
+        self, ) -> set_tped_nped.SetTemperatureDensityPedestalModel:
+        return set_tped_nped.SetTemperatureDensityPedestalModel()
+
+    def build_runtime_params(self,
+                             t: chex.Numeric) -> set_tped_nped.RuntimeParams:
+        return set_tped_nped.RuntimeParams(
+            set_pedestal=self.set_pedestal.get_value(t),
+            n_e_ped=self.n_e_ped.get_value(t),
+            n_e_ped_is_fGW=self.n_e_ped_is_fGW,
+            T_i_ped=self.T_i_ped.get_value(t),
+            T_e_ped=self.T_e_ped.get_value(t),
+            rho_norm_ped_top=self.rho_norm_ped_top.get_value(t),
+        )
+
+PedestalConfig = SetTpedNped
 
 _IMPURITY_MODE_FRACTIONS: Final[str] = 'fractions'
-
-
 @jax.tree_util.register_dataclass
 @dataclasses.dataclass(frozen=True)
 class RuntimeParamsIM:
@@ -4728,8 +4766,7 @@ class ToraxConfig(model_base.BaseModelFrozen):
     neoclassical: Neoclassical0 = Neoclassical0()
     solver: SolverConfig = pydantic.Field(discriminator='solver_type')
     transport: QLKNNTransportModel = pydantic.Field(discriminator='model_name')
-    pedestal: pedestal_pydantic_model.PedestalConfig = pydantic.Field(
-        discriminator='model_name')
+    pedestal: PedestalConfig = pydantic.Field(discriminator='model_name')
 
 
 CONFIG = {
