ab9a893c85accc4018723823f2be3dff5b6e42cd
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-10 00:31:21 -0400

    up

--- run.py
+++ run.py
@@ -1229,34 +1229,6 @@ class GeometrySource(enum.Enum):
     FBT = 1
     EQDSK = 2
 
-
-def _load_CHEASE_data(file_path):
-    with open(file_path, 'r') as file:
-        chease_data = {}
-        var_labels = file.readline().strip().split()[1:]
-        for var_label in var_labels:
-            chease_data[var_label] = []
-        for line in file:
-            values = line.strip().split()
-            for var_label, value in zip(var_labels, values):
-                chease_data[var_label].append(float(value))
-    return {
-        var_label: np.asarray(chease_data[var_label])
-        for var_label in chease_data
-    }
-
-
-def get_geometry_dir(geometry_dir):
-    if geometry_dir is None:
-        geometry_dir = "geo"
-    return geometry_dir
-
-
-def load_geo_data(geometry_dir, geometry_file, geometry_source):
-    geometry_dir = get_geometry_dir(geometry_dir)
-    filepath = os.path.join(geometry_dir, geometry_file)
-    return _load_CHEASE_data(file_path=filepath)
-
 @jax.tree_util.register_dataclass
 @dataclasses.dataclass(frozen=True)
 class ConstantGeometryProvider:
@@ -4625,9 +4597,21 @@ class StandardGeometryIntermediates:
         a_minor: float,
         B_0: float,
         hires_factor: int,
-    ) -> typing_extensions.Self:
-        chease_data = load_geo_data(geometry_directory, geometry_file,
-                                    GeometrySource.CHEASE)
+    ):
+        file_path = os.path.join("geo", "ITER_hybrid_citrin_equil_cheasedata.mat2cols")
+        with open(file_path, 'r') as file:
+            chease_data = {}
+            var_labels = file.readline().strip().split()[1:]
+            for var_label in var_labels:
+                chease_data[var_label] = []
+            for line in file:
+                values = line.strip().split()
+                for var_label, value in zip(var_labels, values):
+                    chease_data[var_label].append(float(value))
+        chease_data = {
+            var_label: np.asarray(chease_data[var_label])
+            for var_label in chease_data
+        }
         psiunnormfactor = R_major**2 * B_0
         psi = chease_data['PSIchease=psi/2pi'] * psiunnormfactor * 2 * np.pi
         Ip_chease = (chease_data['Ipprofile'] / g.mu_0 * R_major * B_0)
