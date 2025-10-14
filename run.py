1f655b0510e57aa7c2d3fcb7f942e3e2e44ce699
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-13 20:17:45 -0400

    unsued ions

--- run.py
+++ run.py
@@ -210,7 +210,6 @@ _TOLERANCE: Final[float] = 1e-6
 @dataclasses.dataclass(frozen=True)
 class IonProperties:
     symbol: Any
-    name: Any
     A: Any
     Z: Any
 
@@ -225,16 +224,9 @@ g.mu_0 = 4 * jnp.pi * 1e-7
 g.k_B = 1.380649e-23
 g.eps = 1e-7
 ION_PROPERTIES: Final[tuple[IonProperties, ...]] = (
-    IonProperties(symbol="D", name="Deuterium", A=2.0141, Z=1.0),
-    IonProperties(symbol="T", name="Tritium", A=3.0160, Z=1.0),
-    IonProperties(symbol="C", name="Carbon", A=12.011, Z=6.0),
-    IonProperties(symbol="N", name="Nitrogen", A=14.007, Z=7.0),
-    IonProperties(symbol="O", name="Oxygen", A=15.999, Z=8.0),
-    IonProperties(symbol="Ne", name="Neon", A=20.180, Z=10.0),
-    IonProperties(symbol="Ar", name="Argon", A=39.95, Z=18.0),
-    IonProperties(symbol="Kr", name="Krypton", A=83.798, Z=36.0),
-    IonProperties(symbol="Xe", name="Xenon", A=131.29, Z=54.0),
-    IonProperties(symbol="W", name="Tungsten", A=183.84, Z=74.0),
+    IonProperties(symbol="D", A=2.0141, Z=1.0),
+    IonProperties(symbol="T", A=3.0160, Z=1.0),
+    IonProperties(symbol="Ne", A=20.180, Z=10.0),
 )
 ION_PROPERTIES_DICT: Final[Mapping[
     str, IonProperties]] = immutabledict.immutabledict(
