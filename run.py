f215b8bce7f6e55fa7da050a967a23cad82d3bb9
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-10-15 18:50:15 -0400

    Refactor state variables into single array s with numpy slicing (like weather.py): s[g.T_i_slice], s[g.T_e_slice], s[g.psi_slice], s[g.n_e_slice]

--- run.py
+++ run.py
@@ -962,14 +962,21 @@ g.psi_bc = (None, None, 0.0, g.dpsi_drhonorm_edge)
 
 g.num_cells = g.n_rho
 g.num_channels = 4
+n = g.num_cells
+g.T_i_slice = np.s_[:n]
+g.T_e_slice = np.s_[n:2*n]
+g.psi_slice = np.s_[2*n:3*n]
+g.n_e_slice = np.s_[3*n:4*n]
+g.state_size = 4 * n
 g.zero_block = jnp.zeros((g.num_cells, g.num_cells))
 g.zero_vec = jnp.zeros(g.num_cells)
 g.ones_vec = jnp.ones(g.num_cells)
 g.v_face_psi_zero = jnp.zeros_like(g.geo_g2g3_over_rhon_face)
 g.ones_like_vpr = jnp.ones_like(g.geo_vpr)
 
-s.T_i = jnp.interp(g.cell_centers, g.T_i_profile_x, g.T_i_profile_y)
-s.T_e = jnp.interp(g.cell_centers, g.T_e_profile_x, g.T_e_profile_y)
+s = jnp.zeros(g.state_size)
+s = s.at[g.T_i_slice].set(jnp.interp(g.cell_centers, g.T_i_profile_x, g.T_i_profile_y))
+s = s.at[g.T_e_slice].set(jnp.interp(g.cell_centers, g.T_e_profile_x, g.T_e_profile_y))
 nGW = g.Ip / 1e6 / (jnp.pi * g.geo_a_minor**2) * g.scaling_n_e
 n_e_value = g.n_e * nGW
 n_e_face = jnp.concatenate([
@@ -983,22 +990,22 @@ nbar_from_n_e_face_inner = (
 dr_edge = g.geo_R_out_face[-1] - g.geo_R_out_face[-2]
 C = (g.nbar * nGW - 0.5 * n_e_face[-1] * dr_edge / a_minor_out) / (
     nbar_from_n_e_face_inner + 0.5 * n_e_face[-2] * dr_edge / a_minor_out)
-s.n_e = C * n_e_value
-s.psi = g.geo_psi_from_Ip_base * (g.Ip / g.geo_Ip_profile_face_base[-1])
-s.t = 0.0
-history = [(s.t, s.T_i, s.T_e, s.psi, s.n_e)]
+s = s.at[g.n_e_slice].set(C * n_e_value)
+s = s.at[g.psi_slice].set(g.geo_psi_from_Ip_base * (g.Ip / g.geo_Ip_profile_face_base[-1]))
+t = 0.0
+history = [(t, s[g.T_i_slice], s[g.T_e_slice], s[g.psi_slice], s[g.n_e_slice])]
 while True:
-    psi_face_grad = compute_face_grad(s.psi, g.psi_bc[0], g.psi_bc[1], g.psi_bc[2], g.psi_bc[3])
+    psi_face_grad = compute_face_grad(s[g.psi_slice], g.psi_bc[0], g.psi_bc[1], g.psi_bc[2], g.psi_bc[3])
     current_q_face = jnp.concatenate([
         jnp.expand_dims(jnp.abs(g.q_factor_axis * g.dx_array / psi_face_grad[1]), 0),
         jnp.abs(g.q_factor_bulk * g.face_centers[1:] / psi_face_grad[1:]),
     ])
-    ions_for_sources = get_updated_ions(s.n_e, s.T_e)
+    ions_for_sources = get_updated_ions(s[g.n_e_slice], s[g.T_e_slice])
     core_transport = calculate_transport_coeffs(
-        s.T_i,
-        s.T_e,
-        s.n_e,
-        s.psi,
+        s[g.T_i_slice],
+        s[g.T_e_slice],
+        s[g.n_e_slice],
+        s[g.psi_slice],
         ions_for_sources.n_i,
         ions_for_sources.n_i_bc,
         ions_for_sources.n_impurity,
@@ -1016,10 +1023,10 @@ while True:
         g.chi_timestep_prefactor * basic_dt,
         g.max_dt,
     )
-    crosses_t_final = (s.t < g.t_final) * (s.t + initial_dt > g.t_final)
+    crosses_t_final = (t < g.t_final) * (t + initial_dt > g.t_final)
     initial_dt = jax.lax.select(
         crosses_t_final,
-        g.t_final - s.t,
+        g.t_final - t,
         initial_dt,
     )
     dt = initial_dt
@@ -1032,10 +1039,10 @@ while True:
         n_i_bound_right = g.n_e_right_bc * dilution_factor_edge
         n_impurity_bound_right = (g.n_e_right_bc -
                                   n_i_bound_right * Z_i_edge) / Z_impurity_edge
-        x_T_i = (s.T_i, g.dx_array, g.T_i_bc)
-        x_T_e = (s.T_e, g.dx_array, g.T_e_bc)
-        x_psi = (s.psi, g.dx_array, g.psi_bc)
-        x_n_e = (s.n_e, g.dx_array, g.n_e_bc)
+        x_T_i = (s[g.T_i_slice], g.dx_array, g.T_i_bc)
+        x_T_e = (s[g.T_e_slice], g.dx_array, g.T_e_bc)
+        x_psi = (s[g.psi_slice], g.dx_array, g.psi_bc)
+        x_n_e = (s[g.n_e_slice], g.dx_array, g.n_e_bc)
         x_initial = (x_T_i, x_T_e, x_psi, x_n_e)
         x_new = x_initial
         tc_in_old = None
@@ -1312,7 +1319,7 @@ while True:
         is_nan_next_dt = jnp.isnan(dt)
         solver_did_not_converge = solver_outputs == 1
         at_exact_t_final = jnp.allclose(
-            s.t + dt,
+            t + dt,
             g.t_final,
         )
         next_dt_too_small = dt < g.min_dt
@@ -1321,24 +1328,24 @@ while True:
         if not (take_another_step & ~is_nan_next_dt):
             break
     result = loop_output
-    s.t = s.t + result[1]
-    s.T_i = result[0][0][0]
-    s.T_e = result[0][1][0]
-    s.psi = result[0][2][0]
-    s.n_e = result[0][3][0]
-    history.append((s.t, s.T_i, s.T_e, s.psi, s.n_e))
-    if s.t >= (g.t_final - g.tolerance):
+    t = t + result[1]
+    s = s.at[g.T_i_slice].set(result[0][0][0])
+    s = s.at[g.T_e_slice].set(result[0][1][0])
+    s = s.at[g.psi_slice].set(result[0][2][0])
+    s = s.at[g.n_e_slice].set(result[0][3][0])
+    history.append((t, s[g.T_i_slice], s[g.T_e_slice], s[g.psi_slice], s[g.n_e_slice]))
+    if t >= (g.t_final - g.tolerance):
         break
 
 
 t_history, *var_histories = zip(*history)
 var_names = ("T_i", "T_e", "psi", "n_e")
 var_bcs = (g.T_i_bc, g.T_e_bc, g.psi_bc, g.n_e_bc)
-t = np.array(t_history)
+t_out = np.array(t_history)
 rho = np.concatenate([[0.0], np.asarray(g.cell_centers), [1.0]])
-(nt, ) = np.shape(t)
+(nt, ) = np.shape(t_out)
 with open("run.raw", "wb") as f:
-    t.tofile(f)
+    t_out.tofile(f)
     rho.tofile(f)
     for var_name, var_bc, var_history in zip(var_names, var_bcs, var_histories):
         var_data = []
@@ -1356,7 +1363,7 @@ with open("run.raw", "wb") as f:
         lo = np.min(var).item()
         hi = np.max(var).item()
         for j, idx in enumerate([0, nt // 4, nt // 2, 3 * nt // 4, nt - 1]):
-            plt.title(f"time: {t[idx]:8.3e}")
+            plt.title(f"time: {t_out[idx]:8.3e}")
             plt.axis([None, None, lo, hi])
             plt.plot(rho, var[idx], "o-")
             plt.savefig(f"{var_name}.{j:04d}.png")
